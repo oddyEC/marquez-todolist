2864238b8c5433d5814c50e67d915acd43fa2a02
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-12-01 10:32:38 -0500

    handleSubmit

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -3,8 +3,13 @@ import { useState } from "react";
 
 const FormTodo = () => {
   const [descripcion, setDescripcion] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault(); //Evitar que se refresque la pagina
+    console.log(descripcion);
+    setDescripcion("")
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="todo-list">
         <div className="file-input">
           <input
